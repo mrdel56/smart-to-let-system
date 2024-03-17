fd8b52ee7963511912cc1daa4d131c4c799ea625
atik65 <atikhasan801@gmail.com>
2024-03-18 05:08:41 +0600

    fix: signup password validation and firstname lastname issues has been fixed

--- mysite/views.py
+++ mysite/views.py
@@ -1,74 +1,91 @@
-from django.shortcuts import render,HttpResponse,redirect
-from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_process
-from django.contrib.auth import logout as logout_process 
+from django.contrib.auth import logout as logout_process
+from django.contrib.auth.models import User
+from django.shortcuts import HttpResponse, redirect, render
+
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def another(request):
-    return render(request, 'another-page.html')
+    return render(request, "another-page.html")
+
 
 def signup(request):
-    if request.method=="POST":
-        uname=request.POST.get("username")
-        fname=request.POST.get("fname")
-        lname=request.POST.get("lname")
-        email=request.POST.get("email")
-        password=request.POST.get("password")
-        confirmpassword=request.POST.get("confirm_password")
-        #print(uname,fname,lname,email,password,confirmpassword)
-        
-        #if password do not match with the confirm password
-        if password != confirmpassword:
-            #return HttpResponse("Incorrect Password!")
-            messages.warning(request, "Invalid password!")
-            return redirect('/signup')
-        
-        #if username is already taken 
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        # print(uname,fname,lname,email,password,confirmpassword)
+
+        # if password do not match with the confirm password
+        if password != confirm_password:
+            # return HttpResponse("Incorrect Password!")
+            messages.error(request, "Invalid password!")
+            return redirect("/signup")
+
+        # if password is less than 8 characters
+        if len(password) < 8:
+            # return HttpResponse("Password is too short!")
+            messages.error(request, "Password should be at least 8 characters long!")
+            return redirect("/signup")
+
+        # if username is already taken
         try:
             if User.objects.get(username=uname):
-                #return HttpResponse("Username already taken")
+                # return HttpResponse("Username already taken")
                 messages.info(request, "Username is already taken!")
-                return redirect('/signup')
-            
+                return redirect("/signup")
+
         except:
             pass
 
-        #if email is already taken
+        # if email is already taken
         try:
             if User.objects.get(email=email):
-                #return HttpResponse("Email already taken")
+                # return HttpResponse("Email already taken")
                 messages.info(request, "Email is already taken!")
-                return redirect('/signup')
-            
+                return redirect("/signup")
+
         except:
             pass
 
-
-        myuser = User.objects.create_user(uname,email,password)
-        myuser.save()
-        #return HttpResponse("Signup Successful")
+        user = User.objects.create_user(
+            username=uname,
+            email=email,
+            password=password,
+            first_name=fname,
+            last_name=lname,
+        )
+        user.save()
+        # return HttpResponse("Signup Successful")
         messages.success(request, "Signup successful please login!")
-        return redirect('/login')
-    return render(request, 'Sign-Up.html')
+        return redirect("/login")
+    return render(request, "Sign-Up.html")
+
 
 def login(request):
-    if request.method=="POST":
-        uname=request.POST.get("username")
-        password=request.POST.get("password")
-        myuser=authenticate(username=uname,password=password)
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        password = request.POST.get("password")
+        myuser = authenticate(username=uname, password=password)
         if myuser is not None:
-            login_process(request,myuser)
+            login_process(request, myuser)
             messages.success(request, "Login Successfully")
-            return redirect('/')
+            return redirect("/")
         else:
             messages.error(request, "Invalid username or password")
-            return redirect('/login')
-    return render(request, 'Login.html')
+            return redirect("/login")
+    return render(request, "Login.html")
+
 
 def logout(request):
     logout_process(request)
-    messages.info(request,"Logout Successful")
-    return redirect('login')
+    messages.info(request, "Logout Successful")
+    return redirect("login")
