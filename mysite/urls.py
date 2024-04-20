17fd6a2579ffaf605cbb61065246bed9bb61a088
atik65 <atikhasan801@gmail.com>
2024-04-21 00:38:22 +0600

    fix: global settings updated

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path,include
-
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,5 +13,8 @@ urlpatterns = [
     path("logout", views.logout, name="logout"),
     path("", include("properties.urls")),
     path("", include("profileapp.urls")),
-]+ static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
