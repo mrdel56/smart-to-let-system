30cad01445786f69bf168ce2cff75a295c9c168e
MR_DEL <del118241@gmail.com>
2024-04-25 00:39:30 +0600

    fixed server problem

--- properties/urls.py
+++ properties/urls.py
@@ -3,16 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("", views.home),
-    path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
-   #path("location-list/", views.location_list, name="location-list"),
-   path("add-property/", views.add_property, name="add-property"),
-=======
     path("", views.property_list, name="property_list"),
     # path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
     path("<id>", views.property_details, name="property_details"),
     path("category/<slug:val>", views.category, name="category"),
-    path("location-list/", views.location_list, name="location-list"),
->>>>>>> 8d67a2f33ec96502128f67916a21890dcb816d86
+    #path("location-list/", views.location_list, name="location-list"),
 ]
--- properties/views.py
+++ properties/views.py
@@ -94,33 +94,33 @@ def property_details(request, id):
 #     return redirect("location_list")
 
 
-def add_property(request):
-    if request.method == "POST":
-        category = request.POST.get("category")
-        rooms = request.POST.get("rooms")
-        washrooms = request.POST.get("washrooms")
-        kitchen = request.POST.get("kitchen")
-        balcony = request.POST.get("balcony")
-        floor = request.POST.get("floor")
-        location = request.POST.get("location")
-        description = request.POST.get("description")
-        price = request.POST.get("price")
-        property_img = request.FILES.get("property_img")
+# def add_property(request):
+#     if request.method == "POST":
+#         category = request.POST.get("category")
+#         rooms = request.POST.get("rooms")
+#         washrooms = request.POST.get("washrooms")
+#         kitchen = request.POST.get("kitchen")
+#         balcony = request.POST.get("balcony")
+#         floor = request.POST.get("floor")
+#         location = request.POST.get("location")
+#         description = request.POST.get("description")
+#         price = request.POST.get("price")
+#         property_img = request.FILES.get("property_img")
 
-        property = Property.objects.create(
-            category=category,
-            rooms=rooms,
-            washrooms=washrooms,
-            kitchen=kitchen,
-            balcony=balcony,
-            floor=floor,
-            location=location,
-            description=description,
-            price=price,
-            property_img=property_img,
-        )
-        property.save()
-        messages.success(request, "Property added successfully!")
-        return redirect("home")
+#         property = Property.objects.create(
+#             category=category,
+#             rooms=rooms,
+#             washrooms=washrooms,
+#             kitchen=kitchen,
+#             balcony=balcony,
+#             floor=floor,
+#             location=location,
+#             description=description,
+#             price=price,
+#             property_img=property_img,
+#         )
+#         property.save()
+#         messages.success(request, "Property added successfully!")
+#         return redirect("home")
 
-    return render(request, "add_property.html")
+#     return render(request, "add_property.html")
