5c2997e4bf38e58247a79f56830f140a87edabb9
atik65 <atikhasan801@gmail.com>
2024-04-26 02:54:57 +0600

    feat: add properties ui done

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("profile/", views.view_profile, name="view-profile"),
     path("edit-profile/", views.edit_profile, name="edit-profile"),
+    path('my-properties/', views.my_properties, name='my-properties'),
 ]
